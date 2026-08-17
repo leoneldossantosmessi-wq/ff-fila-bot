02ed445039bd59818a282ac108101d20845a4e5c
leoneldossantosmessi-wq <leoneldossantosmessi@gmail.com>
2026-08-17 20:17:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -954,24 +954,33 @@ client.on(
                 });
             }
 
-           } catch (error) {
+                   } catch (error) {
 
-    console.error("ERRO NO COMANDO:", error);
+            console.error(
+                "ERRO NO COMANDO:",
+                error
+            );
 
-    if (interaction.deferred || interaction.replied) {
+            if (
+                interaction.deferred ||
+                interaction.replied
+            ) {
 
-        await interaction.editReply({
-            content: "❌ Ocorreu um erro ao executar o comando."
-        }).catch(() => {});
+                await interaction.editReply({
+                    content:
+                        "❌ Ocorreu um erro ao executar o comando."
+                }).catch(() => {});
 
-    } else {
+            } else {
 
-        await interaction.reply({
-            content: "❌ Ocorreu um erro ao executar o comando.",
-            flags: 64
-        }).catch(() => {});
+                await interaction.reply({
+                    content:
+                        "❌ Ocorreu um erro ao executar o comando.",
+                    flags: 64
+                }).catch(() => {});
+            }
+        }
     }
-} 
 );
 
 
